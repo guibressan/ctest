569c17a9a7de998b9b0b24832c5c4ee0caa134c2
Bressan <g.br3ssan@gmail.com>
2024-01-20 23:03:23 +0000

    * Header refactor

--- include/ctest.h
+++ include/ctest.h
@@ -1,40 +1,7 @@
-#ifndef TESTRUNNER_H
-#define TESTRUNNER_H
+#ifndef CTEST_H
+#define CTEST_H
 
-#include <sys/_types/_size_t.h>
-
-typedef enum {
-	CTEST_STATUS_PASS = 0,
-	CTEST_STATUS_FAIL = -1,
-	CTEST_STATUS_FATAL = -2,
-} ctest_status;
-
-typedef struct {
-	const char *failreason;
-	ctest_status status;
-} ctest_result;
-
-typedef ctest_result(*testfunc)(void);
-
-ctest_result ctest_pass(void);
-ctest_result ctest_fail(const char *failreason);
-ctest_result ctest_fatal(const char *failreason);
-
-typedef struct {
-	const char *tname;
-	testfunc fn;
-} ctest_test;
-
-typedef struct {
-	int ntests;
-	size_t alloc_size;
-	ctest_test *tests;
-} ctest_testrunner;
-
-typedef struct {
-	int nfailures;
-	int npasses;
-} ctest_testreport;
+#include "ctest_types.h"
 
 /* testrunner_new creates a new testrunner instance */
 ctest_testrunner ctest_testrunner_new();
@@ -55,4 +22,4 @@ ctest_testreport ctest_testrunner_run(ctest_testrunner *tr);
 /*	tstatus convert the test status to string	*/
 const char *ctest_status_tostr(ctest_status s);
 
-#endif /*	TESTRUNNER_H	*/
+#endif /*	CTEST_H	*/
---
+++ src/ctest_types.h
@@ -0,0 +1,39 @@
+#ifndef CTEST_TYPES_H
+#define CTEST_TYPES_H
+
+#include <sys/_types/_size_t.h>
+
+typedef enum {
+	CTEST_STATUS_PASS = 0,
+	CTEST_STATUS_FAIL = -1,
+	CTEST_STATUS_FATAL = -2,
+} ctest_status;
+
+typedef struct {
+	const char *failreason;
+	ctest_status status;
+} ctest_result;
+
+typedef ctest_result(*testfunc)(void);
+
+ctest_result ctest_pass(void);
+ctest_result ctest_fail(const char *failreason);
+ctest_result ctest_fatal(const char *failreason);
+
+typedef struct {
+	const char *tname;
+	testfunc fn;
+} ctest_test;
+
+typedef struct {
+	int ntests;
+	size_t alloc_size;
+	ctest_test *tests;
+} ctest_testrunner;
+
+typedef struct {
+	int nfailures;
+	int npasses;
+} ctest_testreport;
+
+#endif	/*	CTEST_TYPES_H	*/
